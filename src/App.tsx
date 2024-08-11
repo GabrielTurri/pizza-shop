5d931048626b23f8c8ccf216a10269249829659f
Gabriel <ga-briel.t.a@hotmail.com>
2024-08-11 00:43:07 -0300

    installing React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { router } from './pages/routes'
 
 export function App() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-        <Button>Teste</Button>
+        <RouterProvider router={router} />
       </div>
     </>
   )
